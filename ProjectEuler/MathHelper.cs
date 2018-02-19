ecd0fab8ff5f459421688cc6481e73619718d87b
Kenny Passenier <kennypassenier@gmail.com>
2018-02-19 12:02:25 +0100

    Added a new class called MathHelper to the project.

---
+++ ProjectEuler/MathHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    public static class MathHelper
+    {
+        public static bool IsPalindrome(int number)
+        {
+            string numberAsString = number.ToString();
+            // Requires System.Linq to work. 
+            return numberAsString.SequenceEqual(numberAsString.Reverse());
+        }
+
+        public static List<int> Factors(int number)
+        {
+            List<int> factorList = new List<int>();
+            int limit = (int)Math.Sqrt(number) + 1;
+            for (int possibleFactor = 1; possibleFactor <= limit; possibleFactor++)
+            {
+                if (number % possibleFactor == 0)
+                {
+                    factorList.Add(possibleFactor);
+                    factorList.Add(number / possibleFactor);
+                }
+            }
+            return factorList;
+        }
+    }
+}
--- ProjectEuler/Program.cs
+++ ProjectEuler/Program.cs
@@ -10,7 +10,14 @@ namespace ProjectEuler
     {
         static void Main(string[] args)
         {
-            new Problem1();
+            //new Problem1();
+            //new Problem2();
+            Console.WriteLine(MathHelper.IsPalindrome(515));
+            List<int> test = MathHelper.Factors(38);
+            foreach (var item in test)
+            {
+                Console.WriteLine(item.ToString());
+            }
         }
     }
 }
