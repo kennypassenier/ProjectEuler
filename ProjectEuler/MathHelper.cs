2aa392d045ab19340d32ffef670c14011d7fe24a
Kenny Passenier <kennypassenier@gmail.com>
2018-02-19 14:07:07 +0100

    Updated everything until problem 5.

--- ProjectEuler/MathHelper.cs
+++ ProjectEuler/MathHelper.cs
@@ -35,5 +35,102 @@ namespace ProjectEuler
             }
             return factorList;
         }
+
+        public static bool IsPrime(int number)
+        {
+            if (number < 2)
+            {
+                return false;
+            }
+            else if (number == 2)
+            {
+                return true;
+            }
+            else
+            {
+                int limit = (int)Math.Sqrt(number) + 1;
+                for (int possiblePrime = 3; possiblePrime < limit; possiblePrime++)
+                {
+                    if ((number % possiblePrime) == 0)
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+
+        // Returns a list of factors. Including the number itself. 
+        public static List<long> Factors(long number, bool ordered = false)
+        {
+            List<long> factorList = new List<long>();
+            long limit = (long)Math.Sqrt(number) + 1;
+            for (long possibleFactor = 1; possibleFactor <= limit; possibleFactor++)
+            {
+                if (number % possibleFactor == 0)
+                {
+                    factorList.Add(possibleFactor);
+                    factorList.Add(number / possibleFactor);
+                }
+            }
+            if (ordered)
+            {
+                factorList.Sort();
+            }
+            return factorList;
+        }
+
+        public static bool IsPrime(long number)
+        {
+            if (number < 2)
+            {
+                return false;
+            }
+            else if (number == 2)
+            {
+                return true;
+            }
+            else
+            {
+                int limit = (int)Math.Sqrt(number) + 1;
+                for (int possiblePrime = 3; possiblePrime < limit; possiblePrime++)
+                {
+                    if ((number % possiblePrime) == 0)
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+
+
+        public static List<int> PrimeFactors(int number)
+        {
+            List<int> factoren = Factors(number);
+            foreach (var item in factoren)
+            {
+                if (IsPrime(item) == false)
+                {
+                    factoren.Remove(item);
+                }
+            }
+            return factoren;
+        }
+
+        public static bool IsEvenlyDivisible(int number, int limit)
+        {
+            for (int divisor = 1; divisor < limit + 1; divisor++)
+            {
+                if(number % divisor != 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+              
+
+
     }
 }
---
+++ ProjectEuler/Problem5.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    public class Problem5
+    {
+        public Problem5()
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            int limit = 20;
+            int number = 1;
+            while(MathHelper.IsEvenlyDivisible(number, limit) == false)
+            {
+                number++;
+            }
+            Console.WriteLine(number);
+            sw.Stop();
+            Console.WriteLine(sw.Elapsed);
+        }
+    }
+}
